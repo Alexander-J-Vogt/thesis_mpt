8864646d7340e322c9de214301cfc7146a14163a
Alexander Vogt <alexander@vogt-space.de>
2024-12-03 20:09:00 +0100

    Import A22 & L20 from BSI
    Lending for Housing Purchase and Deposit Liability

--- thesis_mpt/c_program/03_thesis_mpt_databasics_ecb.R
+++ thesis_mpt/c_program/03_thesis_mpt_databasics_ecb.R
@@ -169,8 +169,108 @@ missings <- df_mfi |>
   group_by(year) |> 
   summarise(across(-Column, ~ sum(.), .names = "total_missing_{.col}"), .groups = "drop")
 
+# 04. Balance Sheet Items (BSI) ================================================
+
+# Import bulk download
+df_bsi_raw <- read_csv(paste0(A, "b_ecb/bsi.csv"), col_types = cols(.default = "c"))
+
+df_bsi <- df_bsi_raw |> 
+  filter(REF_AREA %in% euro_cntry_relevant) 
+
+# 04.1 Lending for House Purchases ---------------------------------------------
+df_bsi_a22 <- df_bsi |> 
+  filter(BS_ITEM == "A22") |> # Loan for house purchases 
+  filter(MATURITY_ORIG == "A") |>  # Total Maturity
+  filter(COUNT_AREA == "U6") |> # Concentrate on Domestic Area and not EU Changing Composition (U2)
+  mutate(data_type = case_when(
+    DATA_TYPE == "1" ~ "hp_outst_amount_EUR", # Outstanding Amount at the end of the period (stocks)
+    DATA_TYPE == "4" ~ "hp_fin_transaction_EUR", # Financial Transactions
+    DATA_TYPE == "I" ~ "hp_index_notional_stocks_PPCH" # Index of Notional Stocks
+  ), .after = DATA_TYPE) |> 
+  select(c("REF_AREA", "TIME_PERIOD", "data_type", "OBS_VALUE", "OBS_STATUS")) |> 
+  pivot_wider(names_from = "data_type", 
+              values_from = "OBS_VALUE") |>
+  mutate(across(-c(1:4), as.numeric)) |> 
+  arrange(REF_AREA, TIME_PERIOD) |> 
+  mutate(month = as.Date(paste0(TIME_PERIOD, "-01" )), .after = TIME_PERIOD) |> # Format Data Variable
+  rename(country = REF_AREA) |> 
+  select(-c("TIME_PERIOD", "OBS_STATUS"))
+
+# Filter for years, where countries were actually part of the Eurozone + Exclude the year 2014
+df_bsi_a22 <- df_bsi_a22 |> 
+  filter(month < as.Date("2024-01-01")) |> 
+  filter(!(country == "GR" & month < as.Date("2001-01-01"))) |> # Filter for years with GR being part of the Eurozone
+  filter(!(country == "SI" & month < as.Date("2007-01-01"))) |> # Filter for year with SI being part of the Eurozone
+  filter(!(country == "SK" & month < as.Date("2009-01-01"))) # Filter for years with SK being part of the Eurozone
+
+## 04.2 Deposit Liabilities ----------------------------------------------------
+
+# Create dataset in deposit liabilities and reshape into wide format
+df_bsi_l20 <- df_bsi |> 
+  filter(BS_ITEM == "L20") |> 
+  filter(FREQ == "M") |> # Also data on quarterly basis available
+  filter(COUNT_AREA == "U6") |> # Concentrate on Domestic Area and not EU Changing Composition (U2)
+  filter(BS_COUNT_SECTOR == "2250") |> # Interested in Households and non-profit institutions serving households
+  mutate(data_type = case_when(
+    DATA_TYPE == "1" ~ "hp_outst_amount_EUR", # Outstanding Amount at the end of the period (stocks)
+    DATA_TYPE == "4" ~ "hp_fin_transaction_EUR", # Financial Transactions
+    DATA_TYPE == "I" ~ "hp_index_notional_stocks_PPCH" # Index of Notional Stocks
+  ), .after = DATA_TYPE) |> 
+  select(c("REF_AREA", "TIME_PERIOD", "data_type", "OBS_VALUE")) |> 
+  pivot_wider(names_from = "data_type",
+              values_from = "OBS_VALUE") |> # Put Dataset into wide format
+  mutate(month = as.Date(paste0(TIME_PERIOD, "-01")), .after = TIME_PERIOD) |>  # Format Date Variable
+  mutate(across(c(4:6), as.integer)) |> 
+  rename(country = REF_AREA) |> 
+  arrange(country, month)
+  
+   
+# Filter for years, where countries were actually part of the Eurozone + Exclude the year 2014
+df_bsi_l20 <- df_bsi_l20 |> 
+  filter(month < as.Date("2024-01-01")) |> 
+  filter(!(country == "GR" & month < as.Date("2001-01-01"))) |> # Filter for years with GR being part of the Eurozone
+  filter(!(country == "SI" & month < as.Date("2007-01-01"))) |> # Filter for year with SI being part of the Eurozone
+  filter(!(country == "SK" & month < as.Date("2009-01-01"))) # Filter for years with SK being part of the Eurozone
+
+## 04.3 Total Assets / Liability -----------------------------------------------
+
+# Create dataset for Total Assets / Liabilities and reshape into wide format
+df_bsi_t00 <- df_bsi |> 
+  filter(BS_ITEM == "T00") |> 
+  filter(FREQ == "M")
 
 
+View(df_bsi_l20)
+
+df_bsi_a22 |> 
+  filter(is.na(index_notional_stocks))
+
+  filter(BS_ITEM %in% c("A22", "T00", "L60", "L20")) 
+
+df_bsi_m <- df_bsi |> 
+  filter(FREQ == "M") |> 
+  filter(BS_ITEM %in% c("L60", "T00"))
+
+df_bsi_q <- df_bsi |> 
+  filter(FREQ == "Q")
+
+names <- colnames(df_bsi_t00)[-c(1, 14, 15, 33, 34, 35)]
+lapply(names, function(x) {
+  cat(paste0("Variable: ", x))
+  print(table(df_bsi_t00[[x]]))
+  cat("\n")
+})
+
+table(df_bsi_m$BS_ITEM, df_bsi_m$MATURITY_ORIG)
+table(df_bsi_m$BS_ITEM, df_bsi_m$DATA_TYPE)
+
+table(df_bsi_m$TIME_PERIOD, df_bsi_m$BS_ITEM)
+table(df_bsi_m$TIME_PERIOD, df_bsi_m$DATA_TYPE)
+
+table(df_bsi_a22$REF_AREA, df_bsi_a22$CURRENCY_TRANS)
+
+table(df_bsi_a22$MATURITY_ORIG, df_bsi_a22$CURRENCY_TRANS)
+table(df_bsi_a22$TIME_PERIOD, df_bsi_a22$MATURITY_ORIG)
 
 ###############################################################################+
 ################################# ENDE ########################################+
