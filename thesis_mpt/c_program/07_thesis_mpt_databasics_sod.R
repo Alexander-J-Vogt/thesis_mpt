40ff0848d703d5270712e46ff42cbc05a5b72a8a
Alexander Vogt <alexander@vogt-space.de>
2025-01-16 11:34:06 +0100

    S07: Disable faulty fips check + filter for relevant states

--- thesis_mpt/c_program/07_thesis_mpt_databasics_sod.R
+++ thesis_mpt/c_program/07_thesis_mpt_databasics_sod.R
@@ -34,17 +34,27 @@ files_sod <- files_sod[years >= 1994 & years <= 2024]
 
 # Loop for importing all sod datasets from 1994 to 2024 
 for (i in files_sod) {
-  print(paste0("Iteration Status: ", i))
+  
+  # Extract year
   year <- str_sub(i, start = 5, end = 8)
-  # file <- suppressMessages(read_csv(paste0(A, "f_sod/", i), , col_types = cols(.default = "c")))
-  # SAVE(dfx = file, namex = paste0("sod_", year))
-  # rm(file, year)
+
+  # Import data   
   assign(
     paste0("df_sod_", year),
-    read_csv(paste0(A, "f_sod/", i), , col_types = cols(.default = "c"))
+    read_csv(
+      file = paste0(A, "f_sod/", i), 
+      col_types = cols(.default = "c"),
+      progress = FALSE
+      )
   )
+    
+  # Remover garbage  
+  rm(file, year)
+  gc()
+  
+  # Update Message
+  print(paste0("Iteration Status: ", i))
 }
-
 
 
 ## 1.2 Append all raw SOD files ------------------------------------------------
@@ -86,29 +96,10 @@ for (i in seq_along(sod_temp)) {
 # Create empty list in which all SOD datasets will be saved
 combined_sod <- list()
 
+# Save datasets in file
 for (file in seq_along(sod_temp)) {
   combined_sod[[file]] <- get(sod_temp[file])
 }
-
-
-
-# # Save all datasets within a list object
-# for (file in sod_temp) {
-#   # Read the .rda file
-#   load(paste0(TEMP, "/", file))
-#   
-#   # Extract df name of file
-#   df_name <- str_sub(file, end = -5)
-#   
-#   # Retrieve the df by string
-#   loaded_data <- get(df_name)
-#   
-#   # Combine Data
-#   combined_sod <- append(combined_sod, list(loaded_data))
-#   
-#   # Avoid littering the global environment
-#   rm(loaded_data)
-# }
 
 # Remove all single datasets in order to avoid littering the global enivronment
 rm(list = sod_temp)
@@ -129,29 +120,34 @@ for (i in seq_along(combined_sod)) {
   attr(combined_sod[[i]], "label") <- names_upper[i]
 }
 
-combined_sod |> 
-  filter(year == 2019) |> 
-  View()
+# Save Raw Dataset
+SAVE(dfx = combined_sod, namex = "SOD_raw")
+
+
 ## 1.3 Basic Data Cleaning -----------------------------------------------------
+
+# Import data
+combined_sod <- LOAD(dfinput = "SOD_raw")
+setDT(combined_sod)
 
 # Select the variables of interest
 combined_sod <- combined_sod[, .(year, stcntybr, uninumbr, depsumbr, insured, 
                                  specdesc, msabr, bkmo, stnumbr, cntynumb, rssdid)]
 
 # Clean variables depsumbr and sims_aquired_date from all special characters (This part can be potentially deleted)
-combined_sod <- combined_sod[, depsumbr := gsub(",", "", depsumbr)]
+combined_sod[, depsumbr := gsub(",", "", depsumbr)]
 
 # Format the relevant variables to integers
 columns_to_convert <- c("year", "depsumbr", "msabr", "bkmo")
-combined_sod <- combined_sod[, (columns_to_convert) := lapply(.SD, as.integer), .SDcols = columns_to_convert]
+combined_sod[, (columns_to_convert) := lapply(.SD, as.integer), .SDcols = columns_to_convert]
 
 # Cleaning the variable specdesc from special characters
-combined_sod <- combined_sod[, specdesc := str_to_lower(specdesc)]
-combined_sod <- combined_sod[, specdesc := gsub(" ", "_", specdesc)]
-combined_sod <- combined_sod[, specdesc := gsub(">", "greater", specdesc)]
-combined_sod <- combined_sod[, specdesc := gsub("<", "lower", specdesc)]
-combined_sod <- combined_sod[, specdesc := gsub("-", "_", specdesc)]
-combined_sod <- combined_sod[, specdesc := gsub("\\$", "", specdesc)]
+combined_sod[, specdesc := str_to_lower(specdesc)]
+combined_sod[, specdesc := gsub(" ", "_", specdesc)]
+combined_sod[, specdesc := gsub(">", "greater", specdesc)]
+combined_sod[, specdesc := gsub("<", "lower", specdesc)]
+combined_sod[, specdesc := gsub("-", "_", specdesc)]
+combined_sod[, specdesc := gsub("\\$", "", specdesc)]
 
 # Create fips-code by combining the state and county code
 combined_sod <- combined_sod[stnumbr != "" & cntynumb != ""]
@@ -160,15 +156,13 @@ combined_sod <- FIPSCREATOR(data = combined_sod, state_col = "stnumbr", county_c
 # Excluding the following US territories as they are not relevant for the analysis: 
 # Puerto Rico (72), US Virgin Islands (78), American Samoa (60), 
 # Northern Marian Islands (69), U.S. Minor Outlying Islands (74), Guam (66)
-combined_sod <- combined_sod[!stnumbr %in% c("72", "66", "60", "69", "74", "78")]
-
-# Validate the quality of the fips code by comparing the available fips code in
-# the SOD with a list of all fips code in the US from the US Census Bureau (MDR Education)
-# -> The SOD contains fips-codes that are not existing. The observations with the invalid fips-codes are excluded. 
-# load(paste0(TEMP, "/", "fips_data.rda"))
-fips_data <- LOAD(dfinput = "14_thesis_mpt_databasics_fips")
-notvalid <- setdiff(fips_data$fips, combined_sod$fips)
-combined_sod <- combined_sod[!(fips %in% notvalid)]
+combined_sod <- combined_sod[stnumbr %in% c(
+  "01", "02", "04", "05", "06", "08", "09", "10", "12", "13",
+  "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
+  "25", "26", "27", "28", "29", "30", "31", "32", "33", "34",
+  "35", "36", "37", "38", "39", "40", "41", "42", "44", "45",
+  "46", "47", "48", "49", "50", "51", "53", "54", "55", "56", "11"
+)]
 
 # Copy raw sod 
 raw_sod <- combined_sod
@@ -176,42 +170,43 @@ raw_sod <- combined_sod
 
 ## 1.4 Collapse combined_sod to county-year level ------------------------------
 
-# Restrict the dataset the year 1999 to 2024
-combined_sod <- raw_sod[year >= 1994 & year <= 2024]
-  
-# Only fips-codes, which are observed over the period of 2000 to 2020 are included
-# in the dataset.
-# Collapse data to county-year level
-check_obs <- combined_sod[, .(fips, year, rssdid)]
-check_obs <- check_obs |> distinct(fips, year, rssdid)
-check_obs <- check_obs |> distinct(fips, year)
+# # Restrict the dataset the year 1999 to 2024
+# combined_sod <- raw_sod[year >= 1994 & year <= 2024]
+#   
+# # Only fips-codes, which are observed over the period of 2000 to 2020 are included
+# # in the dataset.
+# # Collapse data to county-year level
+# check_obs <- combined_sod[, .(fips, year, rssdid)]
+# check_obs <- check_obs |> distinct(fips, year, rssdid)
+# check_obs <- check_obs |> distinct(fips, year)
+# 
+# # Irrelevant warning that is supressed. Warning is related to the data.table package.
+# check_obs <- suppressWarnings(check_obs[, ones := 1])
+# 
+# # Determine the counties that are observed over all periods and filter for those counties
+# county_matrix <- dcast(check_obs, fips ~ year, value.var = "ones", fill = 0)
+# filtered_data <- county_matrix[apply(county_matrix == 0, 1, any), ]
+# 
+# setDT(county_matrix)
+# year_nr <- ncol(county_matrix) - 1 # Number of years observed in the restricted dataset
+# counties_full_obs <- county_matrix[rowSums(county_matrix[ , 2:ncol(county_matrix), with = FALSE] > 0) == year_nr]
+# combined_sod <- combined_sod[fips %in% counties_full_obs$fips]
+# 
+# # Rename variables and sort columns
+# setnames(combined_sod, old = c("stnumbr", "cntynumb"), new = c("state", "county"))
+# setcolorder(combined_sod,c("year", "fips", "state"))
+# combined_sod[, stcntybr := NULL]
 
-# Irrelevant warning that is supressed. Warning is related to the data.table package.
-check_obs <- suppressWarnings(check_obs[, ones := 1])
-  
-# Determine the counties that are observed over all periods and filter for those counties
-county_matrix <- dcast(check_obs, fips ~ year, value.var = "ones", fill = 0)
-setDT(county_matrix)
-year_nr <- ncol(county_matrix) - 1 # Number of years observed in the restricted dataset
-counties_full_obs <- county_matrix[rowSums(county_matrix[ , 2:ncol(county_matrix), with = FALSE] > 0) == year_nr]
-combined_sod <- combined_sod[fips %in% counties_full_obs$fips]
-
-# Rename variables and sort columns
-setnames(combined_sod, old = c("stnumbr", "cntynumb"), new = c("state", "county"))
-setcolorder(combined_sod,c("year", "fips", "state"))
-combined_sod[, stcntybr := NULL]
 
 ## 1.5 Collapse raw_sod to bank-county-year level ------------------------------
 
 # Select year and variables 
 raw_sod <- raw_sod[insured == "CB"]
 raw_sod <- raw_sod[, .(year, fips, stnumbr, depsumbr, rssdid)] 
-raw_sod <- raw_sod[year >= 1994 & year <= 2024]
 setcolorder(raw_sod, c("year", "fips", "stnumbr", "rssdid", "depsumbr"))
 
 # Collapse to bank-county-year level
 raw_sod <- raw_sod[, .(depsumcnty = sum(depsumbr)), by = .(year, fips, rssdid)]
-
 
 
 ## 1.6 Save datasets  ----------------------------------------------------------
