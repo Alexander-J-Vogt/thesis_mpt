e83660a9dd4bee6a31b2df8290b9171f2bcc50ea
Alexander Vogt <alexander@vogt-space.de>
2025-01-20 21:28:20 +0100

    S22: unique rows

--- thesis_mpt/c_program/22_thesis_mpt_databasics_ct_crosswalk_file.R
+++ thesis_mpt/c_program/22_thesis_mpt_databasics_ct_crosswalk_file.R
@@ -24,22 +24,26 @@ gc()
 
 # 1. Import Crosswalk file for Conneticut ======================================
 
+# Import 
 df_crosswalk <- read_excel(path = paste0(A, "s_crosswalk_files/ct_cou_to_cousub_crosswalk.xlsx"))
 
+# Clean col names
 col_names <- colnames(df_crosswalk)
 col_names <- str_replace_all(col_names, "\r\n", "_")
 col_names <- str_replace_all(col_names, "\\(", "")
 col_names <- str_replace_all(col_names, "\\)", "")
 colnames(df_crosswalk) <- col_names
 
+# Select new and old FIPS
 df_crosswalk <- df_crosswalk |> 
   filter(!is.na(OLD_COUNTYFP_INCITS31)) |> 
   select(1:2,4) |> 
   mutate(
     fips_old = paste0(STATEFP_INCITS38, OLD_COUNTYFP_INCITS31),
-    fips_new = paste0(STATEFP_INCITS38, NEW_COUNTYFP_INCITS31)
+    fips = paste0(STATEFP_INCITS38, NEW_COUNTYFP_INCITS31)
   ) |> 
-  select(4, 5)
+  select(4, 5) |> 
+  distinct(fips_old, fips)
 
 # SAVE
 SAVE(dfx = df_crosswalk)
