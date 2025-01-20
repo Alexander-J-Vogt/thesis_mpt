26cfc34b39f04552e2e126ff54a9cb77f6d582f4
Alexander Vogt <alexander@vogt-space.de>
2025-01-20 21:26:44 +0100

    S27: Fix change in CT FIPS Code + Extrapolate Landarea

--- thesis_mpt/c_program/27_thesis_mpt_us_varcreation_controls.R
+++ thesis_mpt/c_program/27_thesis_mpt_us_varcreation_controls.R
@@ -24,6 +24,7 @@ gc()
 
 # 1. Summary of Deposits - Control Variables ===================================
 
+## 1.2 Indicator for MSA & Main Office -----------------------------------------
 # Create control variables based on variables available in the SOD
 
 # Load the Summary of Deposits for the period 1994 to 2020
@@ -50,16 +51,16 @@ sod[, `:=` (
 sod <- sod[, d_msa := as.integer(ifelse(msabr > 0, 1, 0))]
 
 # Collapse by year and fips 
-sod <- distinct(sod, year, fips, share_main_office_cnty, nr_main_office_cnty, d_msa)
+sod <- distinct(sod, year, fips, msabr, share_main_office_cnty, nr_main_office_cnty, d_msa)
 
 # SAVE
-SAVE(dfx = sod, name = "controls_sod")
+SAVE(dfx = sod, name = "sod_msa_main_office")
 
 # clear global environment
 rm(list = c("sod"))
 
 
-# 2. Indicator for Firm Size ===================================================
+## 1.2 Indicator for Firm Size -------------------------------------------------
 
 # load dataset
 raw_sod <- LOAD(dfinput = "07_thesis_mpt_databasics_sod")
@@ -92,53 +93,100 @@ raw_sod <- raw_sod %>%
   dplyr::group_by(year, fips) %>%
   dplyr::summarize(d_top_bank = as.integer(any(d_top_bank == 1)), .groups = 'drop')
 
+# Save
+SAVE(dfx = raw_sod, namex= "sod_top5_banks")
+
 
 # 2. Creating Control Dataset ==================================================
 
+# Create a Master Control Dataset
+
 # Import Population County dataset
 pop_cnty <- LOAD(dfinput = "12_thesis_mpt_databasics_us_pop")
+
+# Link New Fips Codes for CT to Old Ones for 2020+
+df_crosswalk_ct <- LOAD(dfinput = "22_thesis_mpt_databasics_ct_crosswalk_file")
+
+pop_cnty <- pop_cnty |> 
+  left_join(df_crosswalk_ct, by = "fips", , relationship = "many-to-many") |> 
+  mutate(fips = if_else(!is.na(fips_old), fips_old, fips)) |> 
+  select(-fips_old)
+
 
 # Import Population State dataset
 pop_state <- LOAD(dfinput = "pop_state")
 
 # Import Unemployment Rate
-ur_cnty <- LOAD(dfinput = "10_thesis_mpt_databasics_us_ur")
+bls_ur <- LOAD(dfinput = "10_thesis_mpt_databasics_us_ur")
+bls_ur$year <- as.numeric(bls_ur$year)
 
 # Import Controls from sod
-controls_sod <- LOAD(dfinput = "controls_sod")
+sod_msa_main_office <- LOAD(dfinput = "sod_msa_main_office")
+sod_top5_banks <- LOAD(dfinput = "sod_top5_banks")
 
 # Population density
-pop_density <- LOAD(dfinput = "13_thesis_mpt_databasics_us_landarea")
+gazette <- LOAD(dfinput = "13_thesis_mpt_databasics_us_landarea")
 
 # Median Household Income and Poverty Rate
 saipe <- LOAD(dfinput = "16_thesis_mpt_databasics_saipe")
 
 # Combining datasets by county and year
-merged_data <- left_join(pop_cnty, controls_sod, by = c("fips", "year"))
-merged_data <- left_join(merged_data, ur_cnty, by = c("fips", "year"))
-merged_data <- left_join(merged_data, pop_density, by = "fips")
-merged_data <- left_join(merged_data, raw_sod, by = c("year", "fips"))
-merged_data <- left_join(merged_data, saipe, by = c("year", "fips"))
+data_merged <- left_join(pop_cnty, sod_msa_main_office, by = c("fips", "year"))
+data_merged <- left_join(data_merged, sod_top5_banks, by = c("fips", "year"))
+data_merged <- left_join(data_merged, bls_ur, by = c("fips", "year"))
+data_merged <- left_join(data_merged, saipe, by = c("fips", "year"))
+data_merged <- left_join(data_merged, gazette, by = c("fips", "year"))
+
+
 
 # 3. Variable Creation =========================================================
-# 
-# # Creating share of employment in each county and year
-# setDT(merged_data)
-# merged_data[, emp_rate := mean_emp / cnty_pop]
-setDT(merged_data)
-# # Calculate population desnity of a county
-merged_data <- merged_data[, pop_density := cnty_pop / landarea_sqkm]
-# 
-# # Creating log emp
-# merged_data <- merged_data[, log_emp := log(mean_emp)]
-# 
-# # Creating log mean_earnings in order to get normally distributed variables
-# merged_data[, log_earnings := log(mean_earning)]
+
+setDT(data_merged)
+
+data_merged <- data_merged[inrange(year, 2004, 2023)]
+
+
+##  Dealing with Missings ---
+
+# For SOD data: If no information in SOD than there wont be any missings information but 
+# there is no branch in this county
+data_merged[, c("share_main_office_cnty", "nr_main_office_cnty", "d_top_bank") := lapply(.SD, function(x) ifelse(is.na(x), 0, x)), 
+            .SDcols = c("share_main_office_cnty", "nr_main_office_cnty", "d_top_bank")]
+
+# Fill landarea data
+data_merged <- data_merged[order(fips, year)] # Ensure data is sorted
+landarea_vars <- c("landarea_sqm", "landarea_smi", "landarea_sqkm", "landarea_sqkm")
+data_merged[,  (landarea_vars) := nafill(.SD, type = "nocb"), .SDcols = landarea_vars, by = fips] # Backward fill within each county
+
+# Calculate population desnity of a county
+data_merged[, pop_density := cnty_pop / landarea_sqkm]
+
+
+
+test_landarea <- data_merged |> filter(is.na(landarea_sqm)) |> filter(year > 2019)
+unique_landare <- unique(test_landarea$fips)
+
+test <- distinct(data_merged, fips, msabr)
+test |> 
+  filter(is.na(msabr)) |> 
+  print()
+
+data_merged |> 
+  filter(is.na(landarea_sqm))
+
+counties <- data_merged |> 
+  filter(is.na(landarea_sqm))
+
+cnty <- unique(counties$fips)
+
+data_merged |> 
+  filter(fips %in% cnty)
+
 DEBUG <- F
 # 4. Imputation of Missings ====================================================
 if (DEBUG) {
 # Basic Imputation of Data
-df_imputation <- merged_data |> 
+df_imputation <- data_merged |> 
   select(-c("state")) |> 
   mutate(fips = as.numeric(fips)) |> 
   select(-c("cnty_main_office", "landarea_sqm", "landarea_sqkm", "fips", "year" ))
@@ -148,13 +196,13 @@ imp <- mice(df_imputation, seed = SEED, print = F)
 test <- complete(imp)
 
 
-test <- merged_data |> 
+test <- data_merged |> 
   mutate(
     ur_imputed = missForest(df_imputation)$ximp$ur
   )
 
 
-missing <- merged_data |> 
+missing <- data_merged |> 
   filter(is.na(ur)) |> 
   arrange(fips) |> 
   mutate(ones = 1) |> 
@@ -164,11 +212,11 @@ missing <- merged_data |>
 # 4. Saving ====================================================================
 
 
-missing <- merged_data |> 
+missing <- data_merged |> 
   filter(is.na(ur)) 
 fips_na <- unique(missing$fips)
 
-test <- merged_data |> 
+test <- data_merged |> 
   filter(fips %in% fips_na)
 
 
