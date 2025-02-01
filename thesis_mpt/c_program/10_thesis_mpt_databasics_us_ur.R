e9d873651798c4d5ace230ad5e627c7206e0b346
Alexander Vogt <alexander@vogt-space.de>
2025-02-01 17:00:34 +0100

    S10: Deleted Import of excel file - NOW API

--- thesis_mpt/c_program/10_thesis_mpt_databasics_us_ur.R
+++ thesis_mpt/c_program/10_thesis_mpt_databasics_us_ur.R
@@ -23,46 +23,6 @@ gc()
 
 # 1. Import Unemployment Data ==================================================
 
-# # Import unemployment data on county level from the economic research service
-# unemp_data <- read_xlsx(paste0(A, "i_economic_research_service/", "Unemployment.xlsx"), skip = 4, col_types = "text")
-# setDT(unemp_data)
-# 
-# # Extract Unemployment Rates for all counties
-# unemp_data <- EXTRACTVAR(data = unemp_data, state_col = "State", county_col = "FIPS_Code", indicator = "Unemployment_rate")
-# 
-# # Standardize variable names
-# colnames(unemp_data) <- str_to_lower(names(unemp_data))
-# setnames(unemp_data, old = c("fips_code"), new = c("fips"))
-# 
-# # Shift data from wide to long format
-# unemp_data <- melt(
-#   unemp_data,
-#   measure.vars = patterns("unemployment_rate_"),
-#   variable.name = "year",
-#   value.name = "ur"
-# )
-# 
-# # Clean the year variable
-# unemp_data <- unemp_data[, year := sub("unemployment_rate_", "", year)]
-# 
-# # Delete observation for states and US Totals
-# unemp_data <- unemp_data[fips != "00000"]
-# unemp_data <- unemp_data[
-#   substr(get("fips"), nchar(get("fips")) - 2, nchar(get("fips"))) != "000", 
-#   .(fips, state, year, ur)
-# ]
-# 
-# # format variables
-# unemp_data <- unemp_data[, year := as.integer(year)]
-# unemp_data <- unemp_data[, ur := round(as.double(ur), 2)]
-# 
-# # Select relevant variable
-# unemp_data <- unemp_data[, c("fips", "year", "ur")]
-# 
-# # Save
-# SAVE(dfx = unemp_data, namex = MAINNAME)
-
-
 # 1.1 API Call for monthly UR --------------------------------------------------
 
 # Get a full list of counties in order to retrieve data
