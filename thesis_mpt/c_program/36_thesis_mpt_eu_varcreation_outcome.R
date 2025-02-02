c2417785f096ca64dd52da90f0babe45aa9dc53e
Alexander Vogt <alexander@vogt-space.de>
2025-02-02 17:27:15 +0100

    S36: Imputed Missing Var + Check plausibility of Imputation

--- thesis_mpt/c_program/36_thesis_mpt_eu_varcreation_outcome.R
+++ thesis_mpt/c_program/36_thesis_mpt_eu_varcreation_outcome.R
@@ -24,85 +24,127 @@ gc()
 
 # 01. Import Data ==============================================================
 
-df_ecb <- LOAD(dfinput = "03_thesis_mpt_databasics_ecb_m")
+ecb <- LOAD(dfinput = "03_thesis_mpt_databasics_ecb_m")
 
 # 02. Select Relevant Variables ================================================
+
 # Create quarter variable # select relevant variables
-df_hp <- df_ecb |> 
+df_ecb <- ecb |> 
   select(country, month, hp_outst_amount_EUR, lending_hp_total_outst_amount,
          lending_hp_over_1_year_outst_amount, lending_hp_over_5_years_outst_amount) |> 
-  mutate(quarter = yq(paste0(year(month), "-Q", quarter(month))), .before = "month") |> 
-  mutate(quarter = as.Date(quarter)) |> 
-  mutate(year = year(month), .after = "country")
+  filter(month >= as.Date("2003-01-01")) |>
+  filter(!(country == "GR" & month < as.Date("2001-01-01"))) |> # Filter for years with GR being part of the Eurozone
+  filter(!(country == "SI" & month < as.Date("2007-01-01"))) |> # Filter for year with SI being part of the Eurozone
+  filter(!(country == "SK" & month < as.Date("2009-01-01"))) |> # Filter for years with SK being part of the Eurozone
+  mutate(
+    country_factor = as.factor(country),
+    month_num = as.numeric(month)
+    ) |> 
+  arrange(country, month)
 
-if (DEBUG) {
-library(naniar)
-# Limit to the period between 2003 and 2023
-df_hp <- df_hp |> 
-  filter(month > as.Date("2002-12-01") & month < as.Date("2024-01-01"))
+# 03. Impute missings via missForest ===========================================
 
-df_ecb <- df_ecb |> 
-  filter(month > as.Date("2002-12-01") & month < as.Date("2024-01-01"))
+# Prepare Dataset for Imputation (no date or character variables)
+df_ecb_impute <- df_ecb |> 
+  select(-c("country", "month"))
 
-miss_var_summary(df_ecb)
-miss_var_summary(df_hp)
+# Impute
+df_ecb_imputed <- missForest(df_ecb_impute)
+df_ecb_imputed_imp <- df_ecb_imputed$ximp
 
-lapply(colnames(df_ecb), function(x) {
-  cat(paste0("Variable: ", x))
-  print(table(is.na(df_ecb[[x]])))
-  cat("\n")
-})
-}
+# Get the data into its original data formats
+df_outcome <- df_ecb_imputed_imp |> 
+  # month to date format & country to character format
+  mutate(
+    month = as.Date(month_num),
+    country = as.character(country_factor)
+  ) |> 
+  # Select and arrange variables
+  arrange(country, month) |> 
+  select(country, month, hp_outst_amount_EUR, lending_hp_total_outst_amount, lending_hp_over_1_year_outst_amount, lending_hp_over_5_years_outst_amount)
+  
+# 03.1 Check Imputations -------------------------------------------------------
 
-# 03. Save =====================================================================
+# Values for BE in lending_hp_over_5_years_outst_amount are imputed (before 2006)
+# Non-parametrical Imputation by missing Forest package
 
-SAVE(dfx = df_hp)
+df_ecb_5years <- df_ecb |> 
+  filter(country %in% c("BE", "DE", "FR", "NL")) |> 
+  select(month, country, lending_hp_over_5_years_outst_amount ) |> 
+  rename(interest_rate_5years = lending_hp_over_5_years_outst_amount )
 
-###############################################################################+
-################################# ENDE ########################################+
-###############################################################################+
+df_outcome_5years <- df_outcome |> 
+  filter(country %in% c("BE", "DE", "FR", "NL")) |>
+  select(country, month, lending_hp_over_5_years_outst_amount) |> 
+  rename(interest_rate_5years_imp = lending_hp_over_5_years_outst_amount)
+
+df_combined <- df_outcome_5years |> 
+  full_join(df_ecb_5years, by = c("country", "month"))
+
+# Imputed Variables
+ggplot(df_combined, aes(x = month, y = interest_rate_5years_imp, color = country)) +
+  geom_point() + 
+  geom_line(data = df_combined %>% filter(!is.na(interest_rate_5years_imp))) +  # Connect only non-missing values
+  labs(title = "Interest rate on 5 years lending",
+       x = "Month", 
+       y = "Interest Rate") +
+  theme_minimal()
+
+# Non-Imputed Variables
+ggplot(df_combined, aes(x = month, y = interest_rate_5years, color = country)) +
+  geom_point() + 
+  geom_line(data = df_combined %>% filter(!is.na(interest_rate_5years))) +  # Connect only non-missing values
+  labs(title = "Interest rate on 5 years lending",
+       x = "Month", 
+       y = "Interest Rate") +
+  theme_minimal()
+
+# Values for GR in lending_hp_total_outst_amount are imputed
+# High spike in imputed interest rates seem to be reasonable compared ES and IT
+# which are more comparable than the Northern Eurzone Countries
+
+df_ecb_total <- df_ecb |> 
+  filter(country %in% c("GR", "DE", "FR", "NL", "IT", "ES")) |> 
+  select(month, country, lending_hp_total_outst_amount ) |> 
+  rename(interest_rate_total = lending_hp_total_outst_amount )
+
+df_outcome_total <- df_outcome |> 
+  filter(country %in% c("GR", "DE", "FR", "NL", "IT", "ES")) |>
+  select(country, month, lending_hp_total_outst_amount) |> 
+  rename(interest_rate_total_imp = lending_hp_total_outst_amount)
+
+df_combined_total <- df_outcome_total |> 
+  full_join(df_ecb_total, by = c("country", "month"))
+
+# Imputed Variables
+ggplot(df_combined_total, aes(x = month, y = interest_rate_total_imp, color = country)) +
+  geom_point() + 
+  geom_line(data = df_combined_total %>% filter(!is.na(interest_rate_total_imp))) +  # Connect only non-missing values
+  labs(title = "Interest rate on total lending | Not Imputed",
+       x = "Month", 
+       y = "Interest Rate") +
+  theme_minimal()
+
+# Non-Imputed Variables
+ggplot(df_combined_total, aes(x = month, y = interest_rate_total, color = country)) +
+  geom_point() + 
+  geom_line(data = df_combined_total %>% filter(!is.na(interest_rate_total))) +  # Connect only non-missing values
+  labs(title = "Interest rate on total lending | Not Imputed",
+       x = "Month", 
+       y = "Interest Rate") +
+  theme_minimal()
+
+# 04. Format Variables =========================================================
+
+# Get into decimal format
+df_main <- df_outcome |> 
+  mutate(across(4:6, ~ { . / 100 } ))
+  
+
+# 05. Save =====================================================================
+
+SAVE(dfx = df_main)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###############################################################################+
 ################################# ENDE ########################################+
 ###############################################################################+
