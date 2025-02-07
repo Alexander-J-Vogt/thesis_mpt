c2d02dbac9e968de72729c727663316ae4ba3fb6
Alexander Vogt <alexander@vogt-space.de>
2025-02-07 13:54:17 +0100

    S70: Setup Implementation of t-LAHR in CREATE PANEL DATASET

--- thesis_mpt/c_program/70_thesis_mpt_function_lp.R
+++ thesis_mpt/c_program/70_thesis_mpt_function_lp.R
@@ -439,9 +439,205 @@ GET_ROBUST_COV_PANEL <- function(panel_results, specs){
 
 ## EXTENSION OF CREATE LIN PANEL #### ------------------------------------------
 
+create_panel_data <- function(specs, data_set){
+  
+  if (DEBUG)
+  
+  
+  # Function ---
+  
+  # Function to compute first differences
+  diff_function <- function(data) {
+    
+    c(NA, diff(data))
+  
+  }
+  
+  # Function to compute cumulative endogenous variables
+  cumul_function <- function(data, hor) {
+    
+    return(dplyr::lead(data, hor) - dplyr::lag(data, 1))
+  
+  }
+  
+  # Prepare list for endogenous variables
+  y_data <- vector("list", specs$hor)
+  
+  # Cumulative Multiplier ---
+  
+  # Create horizons of dependent variables based on whether to use cumulative multipliers
+  if(isTRUE(specs$cumul_mult)){
+    
+    # Loop to create endogenous variables
+    for(ii in 0:(specs$hor-1)){
+      
+      # Create cumulative endogenous vector
+      y_data[[ii + 1]]     <-  data_set %>%
+        dplyr::select(cross_id, date_id, specs$endog_data) |> 
+        dplyr::group_by(cross_id)                          |> 
+        dplyr::mutate_at(vars(specs$endog_data),
+                         list(~cumul_function(., ii)))     |> 
+        dplyr::ungroup()
+    }
+    
+  }    else     {
+    
+    
+    # Loop to create endogenous variables
+    for(ii in 0:(specs$hor-1)){
+      
+      # Create lead endogenous vectors
+      y_data[[ii + 1]]     <-  data_set |> 
+        dplyr::select(cross_id, date_id, specs$endog_data)  |> 
+        dplyr::group_by(cross_id)                  |> 
+        dplyr::mutate_at(vars(specs$endog_data ),
+                         list(~dplyr::lead(., ii))) |> 
+        dplyr::ungroup()
+    }
+    
+  }
+  
+  # Shock Variable ---
+  
+  # Prepare shock variable
+  x_reg_data <- data_set  |>  
+    dplyr::select(cross_id, date_id, specs$shock)
+  
+  
+  # Take first differences of shock variable?
+  if(isTRUE(specs$diff_shock)){
+    
+    x_reg_data    <- x_reg_data                                 |> 
+      dplyr::group_by(cross_id)                                 |> 
+      dplyr::mutate_at(vars(specs$shock), diff_function)        |> 
+      dplyr::rename_at(vars(specs$shock), list(~paste0("d",.))) |> 
+      dplyr::ungroup()
+    
+    # Rename shock variable
+    specs$shock   <- colnames(x_reg_data)[which(!(colnames(x_reg_data) %in% c("cross_id", "date_id")))]
+    
+  }
+  
+  # Exogenous Data ---
+  
+  # Prepare exogenous data
+  x_data <- data_set |> 
+    dplyr::select(cross_id, date_id, specs$exog_data)
+  
+  # Choose exogeonus data with contemporaneous impact
+  if(!is.null(specs$c_exog_data)){
+    
+    c_x_data      <- x_data   %>%
+      dplyr::select(cross_id, date_id, specs$c_exog_data)
+    
+    # Use lagged contemporaneous data as regressors
+    x_reg_data    <- suppressMessages(x_reg_data %>%
+                                        dplyr::left_join(c_x_data))
+    
+  }
+  
+  # Create lagged exogenous data
+  if(!is.null(specs$l_exog_data)){
+    
+    # Make lag sequence
+    lags_exog      <- seq(specs$lags_exog_data)
+    
+    # Lag function
+    lag_functions  <- lapply(lags_exog, function(x) function(col) dplyr::lag(col, x))
+    
+    # Make labels for lagged variables
+    col_lag_labels <- paste(unlist(lapply(specs$l_exog_data, rep, max(lags_exog))), "_lag_", lags_exog, sep = "")
+    
+    # Make and get lagged data
+    l_x_data       <- x_data %>%
+      dplyr::select(cross_id, date_id, specs$l_exog_data)     |> 
+      dplyr::group_by(cross_id)                               |> 
+      dplyr::mutate(across(specs$l_exog_data, lag_functions)) |> 
+      dplyr::ungroup()                                        |> 
+      dplyr::select(-specs$l_exog_data)
+    
+    # Rename columns
+    colnames(l_x_data)[3:dim(l_x_data)[2]] <- col_lag_labels
+    
+    # Use lagged exogenous data as regressor
+    x_reg_data        <- suppressMessages(x_reg_data |> 
+                                            dplyr::left_join(l_x_data))
+    
+  }
+  
+  # Calculate first differences of exogenous data?
+  if(!is.null(specs$c_fd_exog_data) | !is.null(specs$l_fd_exog_data)){
+    
+    d_x_data       <- x_data                                             |> 
+      dplyr::group_by(cross_id)                                          |> 
+      dplyr::mutate_at(vars(-cross_id, -date_id), diff_function)         |> 
+      dplyr::ungroup()                                                   |> 
+      dplyr::rename_at(vars(-cross_id, -date_id), list(~paste0("d",.)))
+    
+  }
+  
+  # Create data with contemporanous impact of first differences
+  if(!is.null(specs$c_fd_exog_data)){
+    
+    # Specify column names to choose
+    specs$c_fd_exog_data <- paste("d", specs$c_fd_exog_data, sep = "")
+    
+    # Create data
+    cd_x_data            <- d_x_data |> 
+      dplyr::select(cross_id, date_id, specs$c_fd_exog_data)
+    
+    
+    # Use first differences as regressors
+    x_reg_data           <- suppressMessages(x_reg_data |> 
+                                               dplyr::left_join(cd_x_data))
+    
+    
+  }
+  
+  
+  # Create lagged exogenous data of first differences
+  if(!is.null(specs$l_fd_exog_data)){
+    
+    # Specify column names to choose
+    specs$l_fd_exog_data <- paste("d", specs$l_fd_exog_data, sep = "")
+    
+    # Make lag sequence
+    dlags_exog      <- seq(specs$lags_fd_exog_data)
+    
+    # Lag function
+    lag_functions  <- lapply(dlags_exog, function(x) function(col) dplyr::lag(col, x))
+    
+    # Make labels for lagged variables
+    col_dlag_labels <- paste(unlist(lapply(specs$l_fd_exog_data, rep, max(dlags_exog))), "_lag_", dlags_exog, sep = "")
+    
+    # Make and get lagged data
+    ld_x_data       <- d_x_data %>%
+      dplyr::select(cross_id, date_id, specs$l_fd_exog_data)     |> 
+      dplyr::group_by(cross_id)                                  |> 
+      dplyr::mutate(across(specs$l_fd_exog_data, lag_functions)) |>  # , .names = "{col_lag_labels}")
+      dplyr::ungroup()                                           |> 
+      dplyr::select(-specs$l_fd_exog_data)
+    
+    
+    # Rename columns
+    colnames(ld_x_data)[3:dim(ld_x_data)[2]] <- col_dlag_labels
+    
+    # Use lags of first differences as regressors
+    x_reg_data    <- suppressMessages(x_reg_data |> 
+                                        dplyr::left_join(ld_x_data))
+  }
+  
+  
+  return(list(x_reg_data = x_reg_data, y_data = y_data, specs = specs))
+}
 
 
-### TEST GROUND ####
+
+
+
+
+
+### TEST GROUND #### -----------------------------------------------------------
 
 df_hp_large_lp <- df_hp_large |> 
   # filter(d_hhi_indicator) |> 
