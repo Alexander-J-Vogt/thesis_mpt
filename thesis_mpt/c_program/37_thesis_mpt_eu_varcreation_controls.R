4f9b8a6b55544a5cd561325f3abb04813d498008
Alexander Vogt <alexander@vogt-space.de>
2025-02-09 23:18:09 +0100

    S37: Add Real GDP monthly; Chow-Lin Interpolation

--- thesis_mpt/c_program/37_thesis_mpt_eu_varcreation_controls.R
+++ thesis_mpt/c_program/37_thesis_mpt_eu_varcreation_controls.R
@@ -31,6 +31,9 @@ df_eurostat <- LOAD(dfinput = "04_thesis_mpt_databasics_eurostat_m")
 
 df_imf <- LOAD(dfinput = "06_thesis_mpt_databasics_imf")
 
+df_eurostat_q <- LOAD(dfinput = "04_thesis_mpt_databasics_eurostat_q")
+
+
 # 02. Relevant Variables =======================================================
 
 # Select relevant variables from the BSI
@@ -43,7 +46,64 @@ df_ecb <- df_ecb |>
          ) 
 
 
+# 03. Real GDP =================================================================
 
+# df_real_gdp <- df_eurostat |>
+#   select(country, month, ip) |> 
+#   full_join(df_eurostat_q, by = c("country", "month")) |> 
+#   mutate(real_gdp = as.double(real_gdp)) |> 
+#   mutate(real_gdp = nafill(real_gdp, type = "locf"))
+
+# install.packages("tsbox")
+library(tsbox)
+
+EA <- c("AT", "BE", "FI", "FR", "DE", "IE", "IT", 
+  "NL", "PT", "ES", "SI", "SK")
+
+list_gdp <- purrr::map(EA, function (x) {
+  
+  # Get data to ts-object
+  df_gdp <- df_eurostat_q |> 
+    filter(country == x) |> 
+    mutate(real_gdp = as.double(real_gdp)) |> 
+    select(real_gdp)
+  
+  ip <- df_eurostat |> 
+    filter(country == x) |> 
+    select(ip)
+  
+  ts_gdp <- ts(data = df_gdp$real_gdp, start = 2003, frequency = 4)
+  
+  ts_ip <- ts(data = ip$ip, start = 2003, frequency = 12)
+  
+  # Use Chow-Lin Interpolation to get monthly real GDP
+  real_gdp <- tempdisagg::td(ts_gdp ~ ts_ip, to = 12, method = "chow-lin-maxlog" )
+  real_gdp_predict <- predict(real_gdp)
+  
+  # Transform back to data frame
+  if (x == "GR") {
+    time <- seq(as.Date("2001-01-01"), as.Date("2023-12-01"), by = "month")
+  } else if (x == "SI") {
+    time <- seq(as.Date("2007-01-01"), as.Date("2023-12-01"), by = "month")
+  } else if (x == "SK") {
+    time <- seq(as.Date("2009-01-01"), as.Date("2023-12-01"), by = "month")
+  } else {
+    time <- seq(as.Date("2003-01-01"), as.Date("2023-12-01"), by = "month")
+  }
+  
+  
+  df_long <- data.frame(
+    country = x,
+    month = time,
+    real_gdp = as.vector(real_gdp_predict)
+  )
+  
+  # Return Value
+  return(df_long)
+
+})
+
+df_gdp <- bind_rows(list_gdp)
 
 
 # 03. Merge to Dataset =========================================================
@@ -52,6 +112,7 @@ df_ecb <- df_ecb |>
 df_controls <- df_ecb |> 
   full_join(df_eurostat, by = c("country", "month")) |> 
   full_join(df_imf, by = c("country", "month")) |> 
+  full_join(df_gdp, by = c("country", "month")) |> 
   filter(month > as.Date("2002-12-01") & month < as.Date("2024-01-01")) |> 
   filter(!(country == "GR" & month < as.Date("2003-01-01"))) |> # Filter for years with GR being part of the Eurozone
   filter(!(country == "SI" & month < as.Date("2007-01-01"))) |> # Filter for year with SI being part of the Eurozone
