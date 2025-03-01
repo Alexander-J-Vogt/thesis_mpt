16805743cfbec0b0188d526890d3f1b96b6cc3fd
Alexander Vogt <alexander@vogt-space.de>
2025-03-01 22:35:26 +0100

    S24: Add Monthly US Monetary Shocks

--- thesis_mpt/c_program/24_thesis_mpt_databasics_monetary_shock.R
+++ thesis_mpt/c_program/24_thesis_mpt_databasics_monetary_shock.R
@@ -33,7 +33,7 @@ df_us_hf <- read_excel(
 )
 
 # Create a yearly monetary shock measure by taking the mean by year
-  df_us_hf_year <- df_us_hf |> 
+df_us_hf_year <- df_us_hf |> 
   mutate(
     date = date(date),
     year = year(date)
@@ -45,20 +45,21 @@ df_us_hf <- read_excel(
     GSS_target_positiv = sum(GSS_target[GSS_target  > 0], na.rm = TRUE),
     GSS_path = sum(GSS_path),
     NS_total = sum(NS),
+    NS_total_mean = mean(NS, na.rm = TRUE),
     NS_negativ = sum(NS[NS  < 0], na.rm = TRUE),
     NS_positiv = sum(NS[NS  > 0], na.rm = TRUE),
   )
   
 
-## 1.2 Jarocinski ==============================================================
+## 1.2 Jarocinski (2024) - Unconvetionaö ==============================================================
 
 # Import Jarocinski's measure on monetary shock (available as one sd shocj and 1 bps shock)
 df_jarocinski_bp <- read_csv(
-  file = paste0(A, "t_policy_shocks_us/Jarocinski_monetary_shock/U1bp.csv")
+  file = paste0(A, "t_policy_shocks_us/Jarocinski_monetary_shock/Jarocinski_2024_Unconventional/U1bp.csv")
 )
 
 df_jarocinski_sd <- read_csv(
-  file = paste0(A, "t_policy_shocks_us/Jarocinski_monetary_shock/U1s.csv")
+  file = paste0(A, "t_policy_shocks_us/Jarocinski_monetary_shock/Jarocinski_2024_Unconventional/U1s.csv")
 )
 
 #' Explanation:
@@ -102,19 +103,82 @@ df_jarocinski_sd <- df_jarocinski_sd |>
     sd_u4 = sum(sd_u4)
   )
 
-# Save U.S. monetary shocks
-df_us_shock <- df_us_hf_year |> 
+
+## 1.3 Jarocinski & Karadi (2020) - Conventional MS ============================
+
+# Import 
+ms_jk <- read_csv(
+  file = paste0(A, "t_policy_shocks_us/Jarocinski_monetary_shock/Jarosinki_Karadi_2020_Conventional/shocks_fed_jk_m.csv"),
+  col_types = cols(.default = "c")
+)
+
+df_ms_jk <- ms_jk |> 
+  # Create month variable
+  mutate(month = if_else(nchar(month) == 1, str_pad(month, width = 2, side = "left", pad = "0"), month)) |> 
+  mutate(month = as.Date(paste0(year, "-", month, "-01"))) |> 
+  # Format shock variable
+  mutate(across(3:ncol(ms_jk), as.numeric)) |> 
+  dplyr::select(-year) |> 
+  group_by(month) |> 
+  mutate(
+    MP_median_total = MP_median,
+    MP_median_positiv = sum(MP_median[MP_median > 0], na.rm = TRUE),
+    MP_median_negativ = sum(MP_median[MP_median < 0], na.rm = TRUE),
+  ) |> 
+  dplyr::select(month, starts_with("MP_median")) |> 
+  mutate(year = year(month), .after = month)
+
+df_ms_jk_annual <- df_ms_jk |> 
+  group_by(year) |> 
+  mutate(
+    MP_median_sum = sum(MP_median_total),
+    MP_median_mean = mean(MP_median_total)
+  ) |> 
+  dplyr::select(year, MP_median_sum, MP_median_mean)
+
+results <- lm(MP_median_total ~ lag(MP_median_total), data = df_ms_jk)
+summary(results)
+
+
+results <- lm(MP_median_mean ~ lag(MP_median_mean), data = df_ms_jk_annual)
+summary(results)
+
+results <- lm(MP_median_sum ~ lag(MP_median_sum), data = df_ms_jk_annual)
+summary(results)
+
+
+## 1.4 Save US Monetary Shocks =================================================
+
+# Save Annual U.S. monetary shocks
+df_us_shock <- df_us_hf_year |>
+  left_join(df_ms_jk_annual, by = "year") |> 
   left_join(df_jarocinski_bp, by = c("year")) |> 
   left_join(df_jarocinski_sd, by = c("year")) |> 
-  select(-c("sd_u2", "sd_u3", "sd_u4", "bp_u2", "bp_u3", "bp_u4", "GSS_path"))
+  dplyr::select(-c("sd_u2", "sd_u3", "sd_u4", "bp_u2", "bp_u3", "bp_u4", "GSS_path")) |> 
+  distinct(year, .keep_all = TRUE)
 
 # Save
 SAVE(dfx = df_us_shock, namex = paste0(MAINNAME, "_us"))
 
+# Save Monthly U.S. Monetary Shocks
+df_us_hf_month <- df_us_hf |> 
+  mutate(month = date(date)) |> 
+  dplyr::select(-date) |> 
+  mutate(month = as.character(month)) |> 
+  mutate(month = paste0(str_sub(month, 1, 7), "-01")) |> 
+  mutate(month = as.Date(month))
 
-# 2. Import Eurzone Measure for Monetary Policy Shoc ===========================
+df_us_shock_monthly <- df_ms_jk |> 
+  left_join(df_us_hf_month, by = "month") |> 
+  mutate(across(everything(), ~ if_else(is.na(.x), 0, .x)))
 
-# 2.1 Altavilla et al. (2019) (with package from Martin Baumgärtner) -----------
+# Save
+SAVE(dfx = df_us_shock_monthly, namex = paste0(MAINNAME, "_us_monthly"))
+
+
+# 2. Import Eur zone Measure for Monetary Policy Shock =========================
+
+## 2.1 Altavilla et al. (2019) (with package from Martin Baumgärtner) -----------
 
 # devtools::install_github("https://github.com/martinbaumgaertner/hfdshocks.git")
 
@@ -132,7 +196,7 @@ df_eurozone_shock <- ecb_shocks(
   return_data_type = "ois"
   )
 
-# Get the measure for the coventional monetary shock
+# Get the measure for the conventional monetary shock
 df_eurozone_target <- data.frame(df_eurozone_shock$factors$release)
 
 # df_eurozone <- df_eurozone_target |> 
